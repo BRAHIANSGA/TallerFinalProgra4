245337adfddee14f65b8f767c9ddfbb2998495a9
BRAHIANSGA <brahian.gil@ucp.edu.co>
2023-11-13 21:02:30 -0500

    rutas

--- backendProgra4/routes/api.php
+++ backendProgra4/routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CompraController;
+use App\Http\Controllers\TypeUserController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('categorias', CategoriaController::class);
+Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('productos', ProductoController::class);
+Route::apiResource('compras', CompraController::class);
+Route::apiResource('typeusers', TypeUserController::class);
+Route::apiResource('users', UserController::class);
