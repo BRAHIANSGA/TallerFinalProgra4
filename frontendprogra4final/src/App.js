9d12e8576a1c2df32227ed252c2e31029eeb77ac
BRAHIANSGA <brahian.gil@ucp.edu.co>
2023-11-16 23:04:46 -0500

    bar

--- frontendprogra4final/src/App.js
+++ frontendprogra4final/src/App.js
@@ -5,8 +5,7 @@ import ProductList from './components/ProductList';
 import ProductCardList from './components/ProductCardList';
 import ClientList from './components/ClientList';
 import CategoryList from './components/CategoryList';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+
 import { Box, CssBaseline, Drawer, Typography } from '@mui/material';
 import PersistentDrawerLeft from './components/Drawer';
 
